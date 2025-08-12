39656839c2b6b9ba02d33dafd3897c00e5aaac1e
Johannes Medagbe <johanmedagbe@gmail.com>
2025-08-12 18:22:51 +0300

    Complete integration SPARQL and GNQA. Isolate templates.

--- rag_script.py
+++ rag_script.py
@@ -4,13 +4,12 @@ Reasoning based on tree-of-thought
 Embedding model = Qwen/Qwen3-Embedding-0.6B
 Generative model = calme-3.2-instruct-78b-Q4_K_S
 Summary model = Phi-3-mini-4k-instruct-fp16
-Requirements: langchain, llama-cpp-python, chroma_db, sparqlwrapper
-python rag_script.py <corpus-text.txt> <generative-model.gguf>
+Requirements: langchain, llama-cpp-python, chroma_db, rdflib
+python rag_script.py
 """
 import click
 import os
 import sys
-import joblib
 
 from pathlib import Path
 from dataclasses import dataclass, field
@@ -27,14 +26,20 @@ from langchain_community.retrievers import BM25Retriever
 from langchain_community.vectorstores import Chroma
 from langchain_core.prompts import PromptTemplate
 
+from rdflib import Graph
+from glob import glob
+
 from tqdm import tqdm
 
+import warnings
 
-CORPUS_TEXT = "/home/johannesm/prelim_project/corpus.txt"
-# XXX: Can we pickle this to make load times faster?
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+# XXX: Remove hard-coded paths
+CORPUS_PATH="/home/johannesm/corpus/"
+
 # XXX: Remove hard-coded paths.
-
-GENERATIVE_MODEL = LlamaCpp(
+GENERATIVE_MODEL=LlamaCpp(
     model_path="/home/johannesm/pretrained_models/calme-3.2-instruct-78b-Q4_K_S.gguf",
     max_tokens=1_000,
     n_ctx=32_768,
@@ -42,10 +47,9 @@ GENERATIVE_MODEL = LlamaCpp(
     temperature=0,
     verbose=False)
 
-# XXX: Can we pickle this to make load times faster?
 # XXX: Remove hard-coded paths.
-SUMMARY_MODEL = LlamaCpp(
-    model_path='/home/johannesm/pretrained_models/Phi-3-mini-4k-instruct-fp16.gguf',
+SUMMARY_MODEL=LlamaCpp(
+    model_path="/home/johannesm/pretrained_models/Phi-3-mini-4k-instruct-fp16.gguf",
     max_tokens=500,
     n_ctx=4096,
     seed=2025,
@@ -54,47 +58,27 @@ SUMMARY_MODEL = LlamaCpp(
 
 
 # Our templates for our simple RAG system
-RAG_TEMPLATE = """
-<s><|user|>
-Relevant information:
-{context}
-History:
-{chat_history}
-Provide a concise answer to the question below. Check first in the history above. If you do not find the answer to the question, use the relevant information above. Do not add any external information.
-Explore different reasoning by emulating a conversation between 3 experts. All experts will write down 1 step of their thinking, then share with the group. Then all experts will go on to the next step and so on. If any expert realizes his wrong at any point, he should leave the conversation.
-The question is:
-{input}
-<|end|>
-<|assistant|>
-"""
+RAG_TEMPLATE, RETRIEVER_TEMPLATE, SUMMARY_TEMPLATE="", "", ""
 
-RETRIEVER_TEMPLATE = """
-<s><|user|>
-Given the following conversation, generate a search query to retrieve relevant documents.
-Conversation:
-{input}
-<|end|>
-<|assistant|>
-"""
+# XX: Remove hard-coded paths.
+RAG_TEMPLATE_PATH="rag_template.txt"
+RETRIEVER_TEMPLATE_PATH="retriever_template.txt"
+SUMMARY_TEMPLATE_PATH="summary_template.txt"
 
-SUMMARY_TEMPLATE = """
-<s><|user|>
-Summarize the conversations and update with the new lines. Be as concise as possible without loosing key information.
-Current summary:
-{summary}
-New lines of conversation:
-{new_lines}
-New summary:<|end|>
-<|assistant|>
-"""
-
-
+with open(RAG_TEMPLATE_PATH) as rag_stream:
+    RAG_TEMPLATE+=rag_stream.read()
+with open(RETRIEVER_TEMPLATE_PATH) as retriever_stream:
+    RETRIEVER_TEMPLATE+=retriever_stream.read()
+with open(SUMMARY_TEMPLATE_PATH) as summary_stream:
+    SUMMARY_TEMPLATE+=summary_stream.read()
+    
 @dataclass
 class GNQNA_RAG():
-    corpus: list
+    corpus_path: str
     rag_template: str
     retriever_template: str
     summary_template: str
+    docs: list = field(init=False)
     memory: Any = field(init=False)
     retrieval_chain: Any = field(init=False)
     ensemble_retriever: Any = field(init=False)
@@ -104,22 +88,23 @@ class GNQNA_RAG():
     chroma_db: Any = field(init=False)
 
     def __post_init__(self):
-        self.chroma_db = self.set_chroma_db(
-            corpus=self.corpus,
+        self.docs=self.corpus_to_docs(self.corpus_path)
+        self.chroma_db=self.set_chroma_db(
+            docs=self.docs,
             embed_model=HuggingFaceEmbeddings(
                 model_name="Qwen/Qwen3-Embedding-0.6B"),
-            db_path='/home/bonfacem/tmp/chroma_db'
+            db_path='/home/johannesm/tmp/chroma_db'
         )
 
         # Init'ing the ensemble retriever
-        bm25_retriever = BM25Retriever.from_texts(self.corpus)
-        bm25_retriever.k = 20   # KLUDGE: Explain why the magic number 20
-        self.ensemble_retriever = EnsembleRetriever(
+        bm25_retriever=BM25Retriever.from_texts(self.docs)
+        bm25_retriever.k=20   # KLUDGE: Explain why the magic number 20
+        self.ensemble_retriever=EnsembleRetriever(
             retrievers=[self.chroma_db.as_retriever(), bm25_retriever],
             weights=[0.3, 0.7])  # KLUDGE: Explain why the magic array
 
         # Init'ing the prompts
-        self.rag_prompt = PromptTemplate(
+        self.rag_prompt=PromptTemplate(
             input_variables=['context', 'question', 'summary'],
             template=self.rag_template)
         self.retriever_prompt = PromptTemplate(
@@ -131,7 +116,7 @@ class GNQNA_RAG():
 
         # Building the modes.
         # KLUDGE: Consider pickling as a cache mechanism
-        self.memory = ConversationSummaryBufferMemory(
+        self.memory=ConversationSummaryBufferMemory(
             llm=SUMMARY_MODEL,
             memory_key='chat_history',
             input_key='input',
@@ -148,20 +133,42 @@ class GNQNA_RAG():
                 llm=GENERATIVE_MODEL,
                 prompt=self.retriever_prompt))
 
-    def set_chroma_db(self, corpus: list,
+    def corpus_to_docs(self, corpus_path: str) -> list:
+        """Convert a corpus into an array of sentences.
+        KLUDGE: XXXX: Corpus of text should be RDF.  This here
+        is for testing.
+        """
+        if not Path(corpus_path).exists():
+            sys.exit(1)
+
+        turtles=glob(f"{corpus_path}rdf_data*.ttl")
+        g=Graph()
+        for turtle in turtles:    
+            g.parse(turtle, format='turtle')
+        docs=[]
+        for subject in set(g.subjects()):
+            text=f"Entity: {subject}\n"
+            for predicate, obj in g.predicate_objects(subject):
+                text+=f"{predicate}: {obj}\n"
+            docs.append(text)
+            return docs[:100_000]
+
+
+        
+    def set_chroma_db(self, docs: list,
                       embed_model: Any, db_path: str,
                       chunk_size: int = 500) -> Any:
         match Path(db_path).exists():
             case True:
-                db = Chroma(
+                db=Chroma(
                     persist_directory=db_path,
                     embedding_function=embed_model
                 )
                 return db
             case _:
-                for i in tqdm(range(0, len(corpus), chunk_size)):
-                    chunk = corpus[i:i+chunk_size]
-                    db = Chroma.from_texts(
+                for i in tqdm(range(0, len(docs), chunk_size)):
+                    chunk=docs[i:i+chunk_size]
+                    db=Chroma.from_texts(
                         texts=chunk,
                         embedding=embed_model,
                         persist_directory=db_path)
@@ -169,13 +176,13 @@ class GNQNA_RAG():
                 return db
 
     def ask_question(self, question: str):
-        memory_var = self.memory.load_memory_variables({})
-        chat_history = memory_var.get('chat_history', '')
-        result = self.retrieval_chain.invoke(
+        memory_var=self.memory.load_memory_variables({})
+        chat_history=memory_var.get('chat_history', '')
+        result=self.retrieval_chain.invoke(
             {'input': question,
              'chat_history': chat_history})
-        answer = result.get("answer")
-        citations = result.get("context")
+        answer=result.get("answer")
+        citations=result.get("context")
         self.memory.save_context(
             {'input': question},
             {'answer': answer})
@@ -186,24 +193,11 @@ class GNQNA_RAG():
         }
 
 query=input('Please enter your query:')
-rag_path='home/johannesm/pretrained_models/rag_demo.pkl'
-
-if Path(rag_path).exists():
-    rag=joblib.load(rag_path)
-    
-    answer = rag.ask_question(query)
-    print(answer['answer'])
-    
-    joblib.dump(rag, rag_path)
-else:
-    rag = GNQNA_RAG(
-    corpus=CORPUS_TEXT,
+rag=GNQNA_RAG(
+    corpus_path=CORPUS_PATH,
     rag_template=RAG_TEMPLATE,
     retriever_template=RETRIEVER_TEMPLATE,
     summary_template=SUMMARY_TEMPLATE,
     )
-
-    answer = rag.ask_question(query)
-    print(answer['answer'])
-    joblib.dump(rag, rag_path)
-
+answer=rag.ask_question(query)
+print(answer['answer'])
