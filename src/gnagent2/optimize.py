daa9be0e7567162ccd55784b266f51b1383f04b4
Johannes Medagbe <johanmedagbe@gmail.com>
2025-11-09 16:37:31 +0300

    Use a better model to improve judgement as well

--- src/gnagent2/optimize.py
+++ src/gnagent2/optimize.py
@@ -14,6 +14,7 @@ evaluate = dspy.Evaluate(
     num_threads=1,
     display_table=True,
     display_progress=True,
+    lm=REFLECTION_MODEL,
 )
 
 evaluate(program)
@@ -36,3 +37,4 @@ optimized_program = optimizer.compile(
 evaluate(optimized_program)
 
 optimized_program.save("optimized_program.json")
+
